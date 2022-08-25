271211573d56e84bca57b64b0be2b5e9d93aa3c8
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-25 10:52:45 +0500

    Add Payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_055052) do
+ActiveRecord::Schema.define(version: 2022_08_25_055215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,14 @@ ActiveRecord::Schema.define(version: 2022_08_25_055052) do
 
   create_table "options", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.datetime "date"
+    t.string "method"
+    t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
