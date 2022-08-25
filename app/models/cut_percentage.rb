1b34ebdef5995f5e9ea2daf5915c4f972a487b2d
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-25 12:13:09 +0500

    Add CutPercentage model

---
+++ app/models/cut_percentage.rb
@@ -0,0 +1,2 @@
+class CutPercentage < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_062654) do
+ActiveRecord::Schema.define(version: 2022_08_25_071212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2022_08_25_062654) do
     t.string "first_name"
     t.string "last_name"
     t.string "contact_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cut_percentages", force: :cascade do |t|
+    t.decimal "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
