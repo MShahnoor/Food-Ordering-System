5afc93c73a8bcea17328b044f2c21d747e1d4dd1
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-25 17:40:31 +0500

    Add association to Menu

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
   has_many :employees
+  has_many :menus
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_120348) do
+ActiveRecord::Schema.define(version: 2022_08_25_123422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,8 @@ ActiveRecord::Schema.define(version: 2022_08_25_120348) do
     t.boolean "is_available"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
   create_table "options", force: :cascade do |t|
@@ -194,4 +196,5 @@ ActiveRecord::Schema.define(version: 2022_08_25_120348) do
 
   add_foreign_key "employees", "employees", column: "manager_id"
   add_foreign_key "employees", "restaurants"
+  add_foreign_key "menus", "restaurants"
 end
