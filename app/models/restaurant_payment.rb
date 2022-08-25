89f3ae5afbd918807b70cbd19d0f2848d7fd0600
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-25 11:27:34 +0500

    Add RestaurantPayment model

---
+++ app/models/restaurant_payment.rb
@@ -0,0 +1,2 @@
+class RestaurantPayment < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_062541) do
+ActiveRecord::Schema.define(version: 2022_08_25_062654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,12 @@ ActiveRecord::Schema.define(version: 2022_08_25_062541) do
   create_table "ratings", force: :cascade do |t|
     t.integer "value"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "restaurant_payments", force: :cascade do |t|
+    t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
