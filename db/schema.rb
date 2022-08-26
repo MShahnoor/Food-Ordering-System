ac7db98a42f82451f643046efae6516862ceb0d3
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-26 19:58:28 +0500

    Remove User model

--- db/schema.rb
+++ db/schema.rb
@@ -218,20 +218,6 @@ ActiveRecord::Schema.define(version: 2022_08_26_110129) do
     t.index ["order_id"], name: "index_service_charge_payments_on_order_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "addons", "restaurants"
   add_foreign_key "employees", "employees", column: "manager_id"
   add_foreign_key "employees", "restaurants"
