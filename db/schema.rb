d0b9d2e9196f01454c3b9d1a15eb3e7874b7c274
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-26 10:41:52 +0500

    Add association to Payment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_053118) do
+ActiveRecord::Schema.define(version: 2022_08_26_053904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,8 @@ ActiveRecord::Schema.define(version: 2022_08_26_053118) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "order_id", null: false
+    t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
   create_table "promos", force: :cascade do |t|
@@ -224,4 +226,5 @@ ActiveRecord::Schema.define(version: 2022_08_26_053118) do
   add_foreign_key "menus", "restaurants"
   add_foreign_key "options", "item_groups"
   add_foreign_key "options", "restaurants"
+  add_foreign_key "payments", "orders"
 end
