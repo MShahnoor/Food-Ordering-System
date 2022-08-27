fc579b82b7990b3e981aed03eebd667a1dfb7b53
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-27 23:45:37 +0500

    Add MenuMenuItem JoinTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_27_181927) do
+ActiveRecord::Schema.define(version: 2022_08_27_184426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,13 @@ ActiveRecord::Schema.define(version: 2022_08_27_181927) do
     t.bigint "menu_id", null: false
     t.index ["menu_id"], name: "index_menu_items_on_menu_id"
     t.index ["restaurant_id"], name: "index_menu_items_on_restaurant_id"
+  end
+
+  create_table "menu_items_menus", id: false, force: :cascade do |t|
+    t.bigint "menu_id", null: false
+    t.bigint "menu_item_id", null: false
+    t.index ["menu_id", "menu_item_id"], name: "index_menu_items_menus_on_menu_id_and_menu_item_id"
+    t.index ["menu_item_id", "menu_id"], name: "index_menu_items_menus_on_menu_item_id_and_menu_id"
   end
 
   create_table "menus", force: :cascade do |t|
