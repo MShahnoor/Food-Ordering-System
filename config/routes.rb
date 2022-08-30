801a52f5279bc6d59c74dd0ea5a35e68073fc939
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-30 18:02:35 +0500

    Remove Admin model and associated route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :employees
   devise_for :customers
-  devise_for :admins
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
