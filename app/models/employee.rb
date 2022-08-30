1ebc4e19f8ddf4189a60fbefd6fff5c53b5d6e2f
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-08-30 18:41:49 +0500

    Integrate DeviseInvitable with Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,8 +1,8 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :invitable, :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+
   belongs_to :restaurant
   has_many :subordinates, class_name: 'Employee',foreign_key: "manager_id"
   belongs_to :manager, class_name: 'Employee', optional: true
