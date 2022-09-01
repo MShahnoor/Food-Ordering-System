0e2e7491a14aedd17b21729800a800202c16849c
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 13:11:48 +0500

    Configure permitted parameters in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def authenticate_inviter!
     authenticate_admin_user!(force: true)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :phone])
   end
 end
