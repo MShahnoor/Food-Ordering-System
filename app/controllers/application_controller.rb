64910526b49dd38386722af85cdac8df7231213f
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 13:13:20 +0500

    Add after_invite method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :phone])
   end
+
+  def after_invite_path_for(resource)
+    admin_employees_path
+  end
 end
