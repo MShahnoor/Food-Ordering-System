a072b7ceb4a73aaa66640f60e9b94e2baea1faf6
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 16:31:04 +0500

    Add index columns to Addon resource in ActiveAdmin

--- app/admin/addons.rb
+++ app/admin/addons.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Addon do
   permit_params :title, :price, :is_available, :restaurant_id
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :price
+    column :is_available
+    actions
+  end
 end
