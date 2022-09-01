8bf8bec9857baec08c7924a9c8f5c945b4c9ddf0
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 13:06:44 +0500

    Add permit params in Employee resource in ActiveAdmin

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Employee do
+  permit_params :email, :password, :password_confirmation,:first_name,:last_name,:restaurant_id
+
   action_item :view_site do
     link_to "Invite Employee", new_employee_invitation_path
   end
