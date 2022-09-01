3b17e59fb9456a05db0e964412f9dc6e98715910
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 16:55:53 +0500

    Add form input fields in Rating resource in ActiveAdmin

--- app/admin/ratings.rb
+++ app/admin/ratings.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Rating do
   permit_params :value, :description, :order_id, :customer_id
+
+  form do |f|
+    inputs "Details" do
+      input :order_id
+      input :customer
+      input :value
+      input :description
+      actions
+    end
+  end
 end
