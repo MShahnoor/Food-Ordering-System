28919599d2ebbbff3a7a09cc01705eb93e756b3d
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-01 17:37:16 +0500

    Add form input fields in ServiceChargePayment resource in ActiveAdmin

--- app/admin/service_charge_payments.rb
+++ app/admin/service_charge_payments.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register ServiceChargePayment do
   permit_params :amount, :order_id
+
+  form do |f|
+    inputs "Details" do
+      input :order_id
+      input :amount
+      actions
+    end
+  end
 end
