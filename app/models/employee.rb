42bcbc3921e84067cc0f548b8123cbd8bfd9b0fd
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-02 14:49:16 +0500

    Make Restaurant reference in Employee optional

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
-  belongs_to :restaurant
+  belongs_to :restaurant,optional: true
   has_many :subordinates, class_name: 'Employee',foreign_key: "manager_id"
   belongs_to :manager, class_name: 'Employee', optional: true
   has_many :orders
