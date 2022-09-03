db378fd332910a3ba14a4b90b9549a5933544a68
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-03 18:12:42 +0500

    Add resource route for Addons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :employees
   devise_for :customers
   devise_for :admins
+  resources :addons
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
