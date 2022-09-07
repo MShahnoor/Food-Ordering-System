f745bb7e4b95fa79131c82b5ec703c2dd00f9654
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-07 12:19:52 +0500

    Add create action and permit params in ItemGroups controller

--- app/controllers/item_groups_controller.rb
+++ app/controllers/item_groups_controller.rb
@@ -10,4 +10,19 @@ class ItemGroupsController < ApplicationController
   def new
     @item_group = ItemGroup.new
   end
+
+  def create
+    @item_group = ItemGroup.new(item_group_params)
+
+    if @item_group.save
+      flash[:notice] = "Created Category!"
+      redirect_to item_groups_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def item_group_params
+    params.require(:item_group).permit(:title, :is_available, :restaurant_id)
+  end
 end
