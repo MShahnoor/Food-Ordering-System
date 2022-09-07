1e50d45c35837155336922f3aa175ab3f638d8f1
Muhammad Shahnoor <shahnoorraza46@gmail.com>
2022-09-07 12:14:56 +0500

    Add show action in ItemGroups controller

--- app/controllers/item_groups_controller.rb
+++ app/controllers/item_groups_controller.rb
@@ -2,4 +2,8 @@ class ItemGroupsController < ApplicationController
   def index
     @item_groups = current_employee.restaurant.item_groups
   end
+
+  def show
+    @item_group = ItemGroup.find(params[:id])
+  end
 end
